ebb99785f1f6c269d84936bb107eaed886925c3b
lash <lash@AGITATO>
2016-10-22 20:48:26 +0200

    Added spray name to player animation

--- Filth/Assets/Scipts/PlayerController.cs
+++ Filth/Assets/Scipts/PlayerController.cs
@@ -3,9 +3,12 @@ using System.Collections;
 
 [RequireComponent(typeof(Animator))]
 [RequireComponent(typeof(Rigidbody2D))]
+
 public class PlayerController : MonoBehaviour {
 
 	[SerializeField] float speed = 1.0f;
+    [SerializeField] GameObject gamecore;
+    SprayController spray_controller;
 
 	Rigidbody2D rb;
 	Animator animator;
@@ -15,6 +18,7 @@ public class PlayerController : MonoBehaviour {
 	{
 		rb = GetComponent<Rigidbody2D> ();
 		animator = GetComponent<Animator> ();
+        spray_controller = gamecore.GetComponent<SprayController>();
 	}
 	
 	// Update is called once per frame
@@ -22,26 +26,52 @@ public class PlayerController : MonoBehaviour {
 	{
 	}
 
-	void FixedUpdate ()
-	{
-		// Compute movement.
-		float moveHorizontal = Input.GetAxis ("Horizontal");
-		float moveVertical = Input.GetAxis ("Vertical");
+    void FixedUpdate()
+    {
+        // Compute movement.
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        string animation_type = "Idle";
 
-		Vector3 movement = new Vector3 (moveHorizontal, moveVertical, 0.0f);
-		rb.velocity = speed * movement;
+        Vector3 movement = new Vector3(moveHorizontal, moveVertical, 0.0f);
+        rb.velocity = speed * movement;
 
-		// Animate
-		if (moveHorizontal > 0) {
-			animator.Play ("Right");
-		} else if (moveHorizontal < 0) {
-			animator.Play ("Left");
-		} else if (moveVertical > 0) {
-			animator.Play ("Back");
-		} else if (moveVertical < 0) {
-			animator.Play ("Front");
-		} else {
-			animator.Play ("Idle");
-		}
-	}
+        // Animate
+        /*	if (moveHorizontal > 0) {
+                animator.Play ("Right");
+            } else if (moveHorizontal < 0) {
+                animator.Play ("Left");
+            } else if (moveVertical > 0) {
+                animator.Play ("Back");
+            } else if (moveVertical < 0) {
+                animator.Play ("Front");
+            } else {
+                animator.Play ("Idle");
+            }
+            */
+
+        if (moveHorizontal > 0)
+        {
+            animation_type = "Right";
+        }
+        else if (moveHorizontal < 0)
+        {
+            animation_type = "Left";
+        }
+        else if (moveVertical > 0)
+        {
+            animation_type = "Back";
+        }
+        else if (moveVertical < 0)
+        {
+            animation_type = "Front";
+        }
+
+        if (spray_controller.isFired() == true)
+        {
+            animation_type += "_spray";
+        }
+
+        animator.Play(animation_type);
+    }
 }
