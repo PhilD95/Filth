b07afb3ae4981b961463e81b9a82bc13ed0f3969
JojOatXGME <johannes.spangenberg@hotmail.de>
2016-10-22 17:58:31 +0200

    Update ContaminationController.cs

--- Filth/Assets/Scipts/ContaminationController.cs
+++ Filth/Assets/Scipts/ContaminationController.cs
@@ -3,19 +3,31 @@ using System.Collections;
 
 public class ContaminationController: MonoBehaviour {
 
-	float contaminationValue = 0.0f;
+	private float contaminationValue = 0.0f;
+	private int stepsSinceDebug = 0;
 
-	float ContaminationValue {
+	public float ContaminationValue {
 		get { return contaminationValue; }
 		set { contaminationValue = value; }
 	}
 
 	// Use this for initialization
-	void Start () {
+	void Start ()
+	{
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+	{
+	}
+
+	void OnGUI()
+	{
+		if (Application.isEditor)  // or check the app debug flag
+		{
+			Rect debugTextRect = new Rect (new Vector2 (10, 10), new Vector2 (400, 40));
+			GUI.Label(debugTextRect, "Contamination: " + contaminationValue);
+		}
 	}
 
 	public void addContamination(float value) {
