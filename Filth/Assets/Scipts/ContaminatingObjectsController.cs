3fa47c77575775fbb630417729132edf1d5441b5
lash <lash@AGITATO>
2016-10-22 15:14:01 +0200

    collisioncontroller player serialize

--- Filth/Assets/Scipts/ContaminatingObjectsController.cs
+++ Filth/Assets/Scipts/ContaminatingObjectsController.cs
@@ -3,13 +3,14 @@ using System.Collections;
 
 public class ContaminatingObjectsController : MonoBehaviour {
 
+    [SerializeField] GameObject Player;
 	// Use this for initialization
 	void Start () {
-	
+	    
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+	    
 	}
 }
