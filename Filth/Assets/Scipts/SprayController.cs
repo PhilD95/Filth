3fd9ee40e4eea85ff71b86091f178def1f5e59e9
lash <lash@AGITATO>
2016-10-22 18:52:57 +0200

    spray code typo

--- Filth/Assets/Scipts/SprayController.cs
+++ Filth/Assets/Scipts/SprayController.cs
@@ -15,13 +15,12 @@ public class SprayController : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
         last_fired_framecount = 0;
-        power = 1.0;
+        power = 1.0f;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	    
 	}
-
-    void
+    
 }
