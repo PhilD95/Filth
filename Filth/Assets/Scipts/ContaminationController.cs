ca886c53d3bbdfde5061454ed436fb2b2c7526b0
JojOatXGME <johannes.spangenberg@hotmail.de>
2016-10-22 15:18:29 +0200

    Implement ContaminationController.cs

--- Filth/Assets/Scipts/ContaminationController.cs
+++ Filth/Assets/Scipts/ContaminationController.cs
@@ -3,13 +3,22 @@ using System.Collections;
 
 public class ContaminationController: MonoBehaviour {
 
+	float contaminationValue = 0.0f;
+
+	float ContaminationValue {
+		get { return contaminationValue; }
+		set { contaminationValue = value; }
+	}
+
 	// Use this for initialization
 	void Start () {
-	
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+	}
+
+	void addContamination(float value) {
+		contaminationValue += value;
 	}
 }
