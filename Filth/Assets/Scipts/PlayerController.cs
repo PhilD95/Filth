2d99c079cd39ac26c747863eae9ccaf93c946edf
JojOatXGME <johannes.spangenberg@hotmail.de>
2016-10-22 18:48:10 +0200

    Update Player

--- Filth/Assets/Scipts/PlayerController.cs
+++ Filth/Assets/Scipts/PlayerController.cs
@@ -1,39 +1,47 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+[RequireComponent(typeof(Animator))]
+[RequireComponent(typeof(Rigidbody2D))]
 public class PlayerController : MonoBehaviour {
 
 	[SerializeField] float speed = 1.0f;
 
 	Rigidbody2D rb;
+	Animator animator;
 
 	// Use this for initialization
-	void Start () {
+	void Start ()
+	{
 		rb = GetComponent<Rigidbody2D> ();
+		animator = GetComponent<Animator> ();
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void Update ()
+	{
 	}
 
 	void FixedUpdate ()
 	{
+		// Compute movement.
 		float moveHorizontal = Input.GetAxis ("Horizontal");
 		float moveVertical = Input.GetAxis ("Vertical");
 
-		Vector3 movement = speed * new Vector3 (moveHorizontal, moveVertical, 0.0f);
-		rb.velocity = movement;
+		Vector3 movement = new Vector3 (moveHorizontal, moveVertical, 0.0f);
+		rb.velocity = speed * movement;
 
-		/*
-		rigidbody.position = new Vector3 
-			(
-				Mathf.Clamp (rigidbody.position.x, boundary.xMin, boundary.xMax), 
-				0.0f, 
-				Mathf.Clamp (rigidbody.position.z, boundary.zMin, boundary.zMax)
-			);
-
-		rigidbody.rotation = Quaternion.Euler (0.0f, 0.0f, rigidbody.velocity.x * -tilt);
-		*/
+		// Animate
+		if (moveHorizontal > 0) {
+			animator.Play ("Right");
+		} else if (moveHorizontal < 0) {
+			animator.Play ("Left");
+		} else if (moveVertical > 0) {
+			animator.Play ("Back");
+		} else if (moveVertical < 0) {
+			animator.Play ("Front");
+		} else {
+			animator.Play ("Idle");
+		}
 	}
 }
