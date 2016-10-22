9842644e27827ffacda188fb5e6c9282f09d13bf
lash <lash@AGITATO>
2016-10-22 18:38:53 +0200

    added collision box contaminationobjects

--- Filth/Assets/Scipts/ContaminatingObjectsController.cs
+++ Filth/Assets/Scipts/ContaminatingObjectsController.cs
@@ -26,7 +26,7 @@ public class ContaminatingObjectsController : MonoBehaviour {
         {
             float decayfactor = (float)(decay * (1.0 - (distancetoplayer / range)));
             contamination_controller.addContamination(strength * decayfactor);
-            Debug.Log("contaminate " + (strength * decayfactor));
+            Debug.Log("contaminate from " + name + ": " + (strength * decayfactor));
         }
 	}
 }
